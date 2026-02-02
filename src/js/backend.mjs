bdcc0efad292e43784ba76c1ec74b2cd33877342
jadbassil <31135113+jadbassil@users.noreply.github.com>
2026-02-02 15:32:36 +0100

    TD 4 first commit

--- src/js/backend.mjs
+++ src/js/backend.mjs
@@ -6,6 +6,17 @@ export async function getEvents() {
     return events;
 }
 
+export async function getEventById(eventId) {
+    try {
+        let event = await pb.collection("events").getOne(eventId);
+        return event;
+    } catch (error) {
+        console.error("Error fetching event by ID:", error);
+        return null;
+    }
+}
+
 export async function getImageUrl(record, imageField) {
     return pb.files.getURL(record, record[imageField]);
 }
+
